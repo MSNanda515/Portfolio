bc55009fe2b881a45b8ac1bf4269b4a37759f763
Meharpreet Singh Nanda <msnanda515@gmail.com>
2021-05-04 20:27:04 -0600

    NavBar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,27 @@
-import react from 'react';
-
+import react, {Component} from 'react';
+import {Container, Row, Col} from 'react-bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './MyStyle.css'
 function Header() {
     return (
         <div>
-            <h1>Meharpreet Singh</h1>    
+            <Container>
+                <Row>
+                    <nav class="navbar navbar-default">
+                        <a class="navbar-brand text-dark" href="#"><strong>Meharpreet Singh Nanda</strong></a>
+                        <ul class="nav navbar-nav navbar-right">
+                            <Row>
+                                <li class="item"><a>page 1</a></li>
+                                <li class="item"><a>page 2</a></li>
+                            </Row>
+                        </ul>
+                    </nav>
+                </Row>
+                
+            </Container>
+        
         </div>
     )   
 }
 
+export default Header
