78e35efe164c76c03064c88969e6f0b67158ba21
Meharpreet Singh Nanda <msnanda515@gmail.com>
2021-05-07 15:05:19 -0600

    Using Styled Components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React from 'react'
+// import { NavbarBrand } from 'react-bootstrap';
 import './App.css';
 import  Navbar from "./components/Navbar/Navbar.js"
 
 import Navbar2 from "./components/Navbar2"
+import { BrowserRouter as Router} from 'react-router-dom'
+
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
+      {/* <Navbar /> */}
+      <Router>
+        <Navbar2 />
+      </Router>
     </div>  
   );
 }
--- src/components/Navbar2/index.js
+++ src/components/Navbar2/index.js
@@ -1,9 +1,42 @@
 import React from 'react';
+import { Nav, NavbarContainer, NavLogo, MobileIcon, NavMenu,
+    NavItem, NavLinks, NavBtn, NavBtnLink} from './NavbarElements';
+import {FaBars} from 'react-icons/fa'
+
+
 
 const Navbar2 = () => {
     return(
-        <div>
-            <h1>Nav</h1>
-        </div>
+        <>
+            <Nav>
+                <NavbarContainer>
+                    <NavLogo to='/'>
+                        dolla
+                    </NavLogo>
+                    <MobileIcon>
+                        <FaBars />
+                    </MobileIcon>
+                    <NavMenu>
+                        <NavItem>
+                            <NavLinks to="about">About</NavLinks>
+                        </NavItem>
+                        <NavItem>
+                            <NavLinks to="discover">Discover</NavLinks>
+                        </NavItem>
+                        <NavItem>
+                            <NavLinks to="services">Services</NavLinks>
+                        </NavItem>
+                        <NavItem>
+                            <NavLinks to="signup">SignUP</NavLinks>
+                        </NavItem>
+                    </NavMenu>
+                    <NavBtn>
+                        <NavBtnLink to="/signin">Sign In</NavBtnLink>
+                    </NavBtn>
+                </NavbarContainer>
+            </Nav>
+        </>
     )
 }
+
+export default Navbar2;
