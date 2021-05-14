89dd5e4353c9bc4e1f7ea9e65ec6341f73214317
Meharpreet Singh Nanda <msnanda515@gmail.com>
2021-05-14 11:51:22 -0600

    Project Section

--- src/components/Services/index.js
+++ src/components/Services/index.js
@@ -10,7 +10,7 @@ import { ServicesContainer, ServicesH1, ServicesWrapper, ServicesCard, ServicesI
 const Services = () => {
     return (
         <ServicesContainer id="services">
-            <ServicesH1>Our Services</ServicesH1>
+            <ServicesH1>Projects</ServicesH1>
             <ServicesWrapper>
                 <ServicesCard>
                     <ServicesIcon src={Icon1}/>
